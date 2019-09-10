b8ab762e6cb932e7a1f6d89c3b2d463b6b8cf2ff
rafael <6116829+rafaelnco@users.noreply.github.com>
2019-09-10 09:12:09 -0300

    base

---
+++ precache-manifest.dd63f23fc368a71eba01bb1bed469de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b49e64f287d59402b727",
+    "url": "/static/css/main.3f7e6d5f.chunk.css"
+  },
+  {
+    "revision": "b49e64f287d59402b727",
+    "url": "/static/js/main.b49e64f2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fa6836c785ac98a61e46",
+    "url": "/static/js/2.fa6836c7.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ddaa0a36ecb85779bf1a77b973bfb667",
+    "url": "/index.html"
+  }
+];
